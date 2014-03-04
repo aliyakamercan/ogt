18e2299413ce7e6a1f88f44f0f58135bb9b46934
Ali Yakamercan <aliyakamercan@gmail.com>
2014-03-04 10:27:58 -0500

    admin page object filtering for store managers

--- shoppingcart/admin.py
+++ shoppingcart/admin.py
@@ -2,8 +2,34 @@ from django.contrib import admin
 from shoppingcart.models import (Store, UserProfile, Product, \
     Order, OrderDetail)
 
+class ProductAdmin(admin.ModelAdmin):
+    def queryset(self, request):
+        user = getattr(request, 'user', None)
+        qs = super(ProductAdmin, self).queryset(request)
+        if user.is_superuser:
+            return qs
+        return qs.filter(store = user.userprofile.store)
+
+class OrderAdmin(admin.ModelAdmin):
+    def queryset(self, request):
+        user = getattr(request, 'user', None)
+        qs = super(OrderAdmin, self).queryset(request)
+        if user.is_superuser:
+            return qs
+        return qs.filter(user__userprofile__store = user.userprofile.store)
+
+class OrderDetailAdmin(admin.ModelAdmin):
+    def queryset(self, request):
+        user = getattr(request, 'user', None)
+        qs = super(OrderDetailAdmin, self).queryset(request)
+        if user.is_superuser:
+            return qs
+        return qs.filter(order__user__userprofile__store = user.userprofile.store)
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderDetail, OrderDetailAdmin)
+
+
 admin.site.register(Store)
 admin.site.register(UserProfile)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderDetail)
